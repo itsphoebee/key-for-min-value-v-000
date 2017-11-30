98a0d54d51ff0d0f4d7790170f36a66c927f9fa8
Phoebe Lau <phoebelau7@gmail.com>
2017-11-30 03:06:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 0
   name_hash.find {|name, value|
-    if value.abs > lowest_value
+    if value.abs < lowest_value
     value  = lowest_value
   end
 }

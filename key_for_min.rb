4a00e3fbcead022b9af191ecdc0fedb91dbc9a00
Phoebe Lau <phoebelau7@gmail.com>
2017-11-30 03:17:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def key_for_min_value(name_hash)
   lowest_value = []
   lowest_key = []
   name_hash.each {|name, value|
-    if value.abs < 100
+    if name_hash.empty? 
+      return nil
+    elsif value.abs < 100
     lowest_value = value
     lowest_key = name
   end

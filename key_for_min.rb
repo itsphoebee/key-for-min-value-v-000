18b776ed790ecf721f98106dcd8b3ee6483608c4
Phoebe Lau <phoebelau7@gmail.com>
2017-11-30 03:21:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_value = 10
+  lowest_value = Float::INFINITY
   lowest_key = []
   if name_hash.empty?
     return nil
